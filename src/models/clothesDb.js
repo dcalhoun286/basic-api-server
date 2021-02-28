dd230ef11f9c7cdbd669b33ff49aee2a0a7b6150
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2021-02-27 22:07:06 -0800

    adding db code files, will incorporate later

---
+++ src/models/clothesDb.js
@@ -0,0 +1,48 @@
+// 'use strict';
+
+// // in-memory database model
+// // this model is meant to be dynamic, meaning it can "get, create, update, delete" any type of resource
+
+// class ClothesModel {
+//   constructor() {
+//     this.id = 0;
+//     this.db = [];
+//   }
+
+//   // READ (CRUD) -> will work for returning all items in our db or a specific item
+//   get(id) {
+//     if (id) {
+//       return this.db.find(record => record.id === id);
+//     } else {
+//       return this.db;
+//     }
+//   }
+
+//   create(obj) {
+//     let record = {
+//       id: ++this.id,
+//       record: obj,
+//     };
+
+//     this.db.push(record);
+//     // the record we've added to the db array
+//     return record;
+//   }
+
+//   // UPDATE
+//   update(id, obj) {
+//     // once we've udpated, we'll return the obj we updated it with
+//     if (id) {
+//       return obj;
+//     }
+//   }
+
+//   // DELETE
+//   delete(id) {
+//     if (id) {
+//       return null;
+//     }
+//   }
+// }
+
+// module.exports = ClothesModel;
