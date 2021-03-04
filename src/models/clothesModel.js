9cee3f24fc5a5a7d59596e251a2dadd5b6b11c13
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2021-03-03 17:33:35 -0800

    GET and POST now work for /clothes route

--- src/models/clothesModel.js
+++ src/models/clothesModel.js
@@ -3,39 +3,40 @@
 // // in-memory database model
 // // this model is meant to be dynamic, meaning it can "get, create, update, delete" any type of resource
 
-// class ClothesModel {
-//   constructor() {
-//     this.id = 0;
-//     this.db = [];
-//   }
+class ClothesModel {
+  constructor() {
+    this.id = 0;
+    this.db = [];
+  }
 
-//   // READ (CRUD) -> will work for returning all items in our db or a specific item
-//   get(id) {
-//     if (id) {
-//       return this.db.find(record => record.id === id);
-//     } else {
-//       return this.db;
-//     }
-//   }
+  //   // READ -> will work for returning all items in our db or a specific item
+  get(id) {
+    if (id) {
+      return this.db.find(record => record.id === id);
+    } else {
+      return this.db;
+    }
+  }
 
-//   create(obj) {
-//     let record = {
-//       id: ++this.id,
-//       record: obj,
-//     };
+  // CREATE
+  create(obj) {
+    let record = {
+      id: ++this.id,
+      record: obj,
+    };
 
-//     this.db.push(record);
-//     // the record we've added to the db array
-//     return record;
-//   }
+    this.db.push(record);
+    // the record we've added to the db array
+    return record;
+  }
 
-//   // UPDATE
-//   update(id, obj) {
-//     // once we've udpated, we'll return the obj we updated it with
-//     if (id) {
-//       return obj;
-//     }
-//   }
+  //   // UPDATE
+  //   update(id, obj) {
+  //     // once we've udpated, we'll return the obj we updated it with
+  //     if (id) {
+  //       return obj;
+  //     }
+  //   }
 
 //   // DELETE
 //   delete(id) {
@@ -43,6 +44,6 @@
 //       return null;
 //     }
 //   }
-// }
+}
 
-// module.exports = ClothesModel;
+module.exports = ClothesModel;
--- src/routes/clothes.js
+++ src/routes/clothes.js
@@ -1,45 +1,41 @@
 'use strict';
 
 const express = require('express');
-// const Clothes = require('../models/clothesModel.js');
+const Clothes = require('../models/clothesModel.js');
 // here, we instantiate the new clothes model
-// const clothing = new Clothes();
+const clothing = new Clothes();
 
 const clothesRouter = express.Router();
 
 // routes
 
 clothesRouter.get('/clothes', getClothes);
-// clothesRouter.get('/clothes/:id', getOneClothingItem);
-// clothesRouter.post('/clothes', createClothingItem);
+clothesRouter.get('/clothes/:id', getOneClothingItem);
+clothesRouter.post('/clothes', createClothingItem);
 // clothesRouter.put('/clothes/:id', updateClothingItem);
 // clothesRouter.delete('/clothes/:id', deleteClothingItem);
 
 // if there are things in the db, this callback will be used to get the thing from the db and send back to the user
 function getClothes(req, res) {
-  // this is CRUD
-  // let all = clothing.get();
-  // this is REST
-  let outputObject = {
-    status: 'OK',
-    msg: 'clothes route',
-  };
-  res.status(200).json(outputObject);
+  // this is CRUD -> get all items from the database
+  let all = clothing.get();
+  // this is REST -> send those items back to the user
+  res.status(200).json(all);
 }
 
-// function getOneClothingItem(req, res) {
+function getOneClothingItem(req, res) {
 // this is coming from a URL, so you have to parse the string to get the number
-//   let id = parseInt(req.params.id);
-//   let item = clothing.get(id);
-//   res.status(200).json(item);
-// }
+  let id = parseInt(req.params.id);
+  let item = clothing.get(id);
+  res.status(200).json(item);
+}
 
-// function createClothingItem(req, res) {
-//   let obj = req.body;
-//   let newItem = clothing.create(obj);
-//   // status 201 is proper status code for creating an item (POST)
-//   res.status(201).json(newItem);
-// }
+function createClothingItem(req, res) {
+  let obj = req.body;
+  let newItem = clothing.create(obj);
+  // status 201 is proper status code for creating an item (POST)
+  res.status(201).json(newItem);
+}
 
 // // localhost:3333/clothes/:id
 // function updateClothingItem(req, res) {
@@ -53,7 +49,7 @@ function getClothes(req, res) {
 //   let id = parseInt(req.params.id);
 //   let deleted = clothing.delete(id);
 //   // status code is 204 for deleting an item; deleted obj is now = null
-//   res.status(204).json(deleted);
+//   res.status(204).send(`ITEM DELETED: ${deleted}`);
 // }
 
 module.exports = clothesRouter;
