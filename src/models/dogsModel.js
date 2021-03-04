83a19a99c6c2f797156a3d6f6840c42f79ed3492
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2021-03-03 16:53:27 -0800

    renamed dependency on dogsModel, and got GET and POST to work

--- src/models/dogsModel.js
+++ src/models/dogsModel.js
@@ -1,51 +1,54 @@
-// 'use strict';
+'use strict';
+
+// const { parse } = require("dotenv/types");
 
 // // in-memory database model
 // // this model is meant to be dynamic, meaning it can "get, create, update, delete" any type of resource
 
-// class DogsModel {
-//   constructor() {
-//     this.id = 0;
-//     this.barks = true;
-//     this.legs = 4;
-//     this.db = [];
-//   }
+class DogsModel {
+  constructor() {
+    this.id = 0;
+    this.barks = true;
+    this.legs = 4;
+    this.db = [];
+  }
 
-//   // READ (CRUD) -> will work for returning all items in our db or a specific item
-//   get(id) {
-//     if (id) {
-//       return this.db.find(record => record.id === id);
-//     } else {
-//       return this.db;
-//     }
-//   }
+  // READ -> will work for returning all items in our db or a specific item
+  get(id) {
+    if (id) {
+      return this.db.find(record => record.id === id);
+    } else {
+      return this.db;
+    }
+  }
 
-//   // CREATE
-//   create(obj) {
-//     let record = {
-//       id: ++this.id,
-//       record: obj,
-//     };
+  // CREATE
+  create(obj) {
+    let record = {
+      id: ++this.id,
+      record: obj,
+    };
 
-//     this.db.push(record);
-//     // the record we've added to the db array
-//     return record;
-//   }
+    this.db.push(record);
+    // the record we've added to the db array
+    return record;
+  }
 
-//   // UPDATE
-//   update(id, obj) {
-//     // once we've updated, we'll return the obj we updated it with
-//     if (id) {
-//       return obj;
-//     }
-//   }
+  // UPDATE
+  // update(id, obj) {
+  //   // once we've updated, we'll return the obj we updated it with
+  //   let id = parseInt(req.params.id);
+  //   if (id) {
+  //     return obj;
+  //   }
+  // }
 
-//   // DELETE
-//   delete(id) {
-//     if (id) {
-//       return null;
-//     }
-//   }
-// }
+  // // DELETE
+  // delete(id) {
+  //   if (id) {
+  //     return null;
+  //   }
+  // }
+}
 
-// module.exports = DogsModel;
+module.exports = DogsModel;
--- src/routes/dogs.js
+++ src/routes/dogs.js
@@ -1,46 +1,43 @@
 'use strict';
 
 const express = require('express');
-// const Dogs = require('../models/dogsDb.js');
+const Dogs = require('../models/dogsModel.js');
 // here, we instantiate the new dogs model
-// const pups = new Dogs();
+const pups = new Dogs();
 
 const dogsRouter = express.Router();
 
 // routes
 
 dogsRouter.get('/dogs', getDogs);
-// dogsRouter.get('/dogs/:id', getOneDog);
-// dogsRouter.post('/dogs', createDog);
+dogsRouter.get('/dogs/:id', getOneDog);
+dogsRouter.post('/dogs', createDog);
 // dogsRouter.put('/dogs/:id', updateDog);
 // dogsRouter.delete('/dogs/:id', deleteDog);
 
-// if there are things in the db, this callback will be used to get the thing from the db and send back to the user
+// if there are things in the db, this callback will be used to get the things from the db and send back to the user
 function getDogs(req, res) {
-  // this is CRUD
-  // let all = pups.get();
-  // this is REST
-  let outputObject = {
-    status: 'OK',
-    msg: 'dogs route',
-  };
-  res.status(200).json(outputObject);
+  // this is CRUD -> get all items from the database
+  let all = pups.get();
+  // this is REST -> send those items back to the user
+  res.status(200).json(all);
 }
 
-// function getOneDog(req, res) {
-//   let id = parseInt(req.params.id);
-//   let item = item.get(id);
-//   res.status(200).json(item);
-// }
+function getOneDog(req, res) {
+  // this is coming from a URL, so you have to parse the string to get the number
+  let id = parseInt(req.params.id);
+  let item = pups.get(id);
+  res.status(200).json(item);
+}
 
-// function createDog(req, res) {
-//   let obj = req.body;
-//   let newItem = pups.create(obj);
-//   // status 201 is proper status code for creating an item (POST)
-//   res.status(201).json(newItem);
-// }
+function createDog(req, res) {
+  let obj = req.body;
+  let newItem = pups.create(obj);
+  // status 201 is proper status code for creating an item (POST)
+  res.status(201).json(newItem);
+}
 
-// // localhost:3333/dogs/:id
+// localhost:3333/dogs/:id
 // function updateDog(req, res) {
 //   let id = parseInt(req.params.id);
 //   let content = req.body;
