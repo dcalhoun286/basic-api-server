1a5895efe8e67ab1fe790ce614fabbdd93cfcf4e
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2021-03-04 16:19:14 -0800

    finished writing all of the tests

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -6,7 +6,7 @@ const { it, expect } = require('@jest/globals');
 const mockReq = supertest(server);
 
 describe('====== SERVER ======', () => {
-  it('/ works', async () => {
+  it('returns a status 200 when visiting \'/\'', async () => {
     await mockReq.get('/')
       .then(result => {
         expect(result.status).toBe(200);
@@ -20,18 +20,121 @@ describe('====== SERVER ======', () => {
       });
   });
 
-  it('/clothes works', async () => {
+  it('returns a status 404 when using a bad method', async () => {
+    await mockReq.post('/')
+      .then(result => {
+        expect(result.status).toBe(404);
+      });
+  });
+
+  it('returns a status 200 when visiting \'/clothes\' route', async () => {
     await mockReq.get('/clothes')
       .then(result => {
         expect(result.status).toBe(200);
       });
   });
 
-  it('/dogs works', async () => {
+  it('can create a new record on \'/clothes\' route using POST', async () => {
+    let response = await mockReq.post('/clothes').send({
+      type: 'shirt',
+      color: 'rainbow',
+    });
+
+    expect(response.status).toBe(201);
+    expect(response.body.id).toBe(1);
+    expect(response.body.record.type).toBe('shirt');
+  });
+
+  it('can read a single record on \'/clothes\' route using GET', async () => {
+    let response1 = await mockReq.post('/clothes').send({
+      type: 'shirt',
+      color: 'rainbow',
+    });
+
+    let response2 = await mockReq.get(`/clothes/${response1.body.id}`);
+
+    expect(response2.status).toBe(200);
+  });
+
+  it('can update a single record on \'/clothes\' route using PUT', async () => {
+    let response1 = await mockReq.post('/clothes').send({
+      type: 'shirt',
+      color: 'rainbow',
+    });
+
+    let response2 = await mockReq.put(`/clothes/${response1.body.id}`).send({
+      color: 'purple',
+    });
+
+    expect(response2.status).toBe(200);
+    expect(response2.body.color).toBe('purple');
+  });
+
+  it('can destroy a record on \'/clothes\' route', async () => {
+    let response1 = await mockReq.post('/clothes').send({
+      type: 'shirt',
+      color: 'rainbow',
+    });
+
+    let response2 = await mockReq.delete(`/clothes/${response1.body.id}`);
+    expect(response2.status).toBe(204);
+    expect(response2.body.id).toBe(undefined);
+  });
+
+  it('returns a status 200 when visiting \'/dogs\' route', async () => {
     await mockReq.get('/dogs')
       .then(result => {
         expect(result.status).toBe(200);
       });
+  });
+
+  it('can create a new record on \'/dogs\' route using POST', async () => {
+    let response = await mockReq.post('/dogs').send({
+      breed: 'husky',
+      color: 'white, black',
+    });
+
+    // console.log('huskyresponse', response.body);
+    expect(response.status).toBe(201);
+    expect(response.body.id).toBe(1);
+    expect(response.body.record.breed).toBe('husky');
+  });
+
+  it('can read a single record on \'/dogs\' route using GET', async () => {
+    let response1 = await mockReq.post('/dogs').send({
+      breed: 'husky',
+      color: 'white, black',
+    });
+
+    let response2 = await mockReq.get(`/dogs/${response1.body.id}`);
+
+    expect(response2.status).toBe(200);
+  });
+
+  it('can update a single record on \'/dogs\' route using PUT', async () => {
+    let response1 = await mockReq.post('/dogs').send({
+      breed: 'husky',
+      color: 'white, black',
+    });
+
+    let response2 = await mockReq.put(`/dogs/${response1.body.id}`).send({
+      breed: 'dalmatian',
+    });
+
+    console.log('shouldsaydal', response2.body);
+    expect(response2.status).toBe(200);
+    expect(response2.body.breed).toBe('dalmatian');
+  });
+
+  it('can destroy a record on \'/dogs\' route', async () => {
+    let response1 = await mockReq.post('/dogs').send({
+      breed: 'husky',
+      color: 'white, black',
+    });
+
+    let response2 = await mockReq.delete(`/dogs/${response1.body.id}`);
+    expect(response2.status).toBe(204);
+    expect(response2.body.id).toBe(undefined);
   });
 
   it('returns a status 500 when an internal server error occurs', async () => {
